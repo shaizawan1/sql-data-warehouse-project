ad4399190677580c72ffa6be91d7d8c088651ddf
Liaqat Ali Shaiz <92707096+shaizawan1@users.noreply.github.com>
2025-07-27 16:40:41 +0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+Go
+use master;
+
+--CREATING DATABASE
+create database Datawarehouse;
+
+--USING DATABASE
+Go
+use Datawarehouse;
+
+
+--CREATING SCHEMAS
+Go
+create schema bronze;
+
+Go
+create schema silver;
+
+Go
+create schema gold;
+
 
