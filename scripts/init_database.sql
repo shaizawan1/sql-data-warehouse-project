665a1c7b272c760e500beafe2876f329429c7843
Liaqat Ali Shaiz <92707096+shaizawan1@users.noreply.github.com>
2025-07-27 16:42:59 +0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,6 +1,14 @@
 Go
 use master;
 
+-- Drop and recreate the 'Datawarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+BEGIN
+    ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawarehouse;
+END;
+GO
+
 --CREATING DATABASE
 create database Datawarehouse;
 
@@ -18,5 +26,4 @@ create schema silver;
 
 Go
 create schema gold;
-
 
