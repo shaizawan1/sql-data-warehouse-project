fe662257b44404fdff9565a433452e279e7c0a85
Liaqat Ali Shaiz <92707096+shaizawan1@users.noreply.github.com>
2025-07-30 17:09:43 +0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -52,4 +52,27 @@ IF OBJECT_ID ( 'bronze.crm_cust_info' , 'U' ) IS NOT NULL
 			sls_quantity INT,
 			sls_price INT
 		);
+		IF OBJECT_ID ( 'bronze.erp_loc_a101' , 'U' ) IS NOT NULL
+		DROP TABLE bronze.erp_loc_a101;
 
+		CREATE TABLE bronze.erp_loc_a101 (
+			cid NVARCHAR(50),
+			cntry NVARCHAR(50)
+		);
+		IF OBJECT_ID ( 'bronze.erp_cust_az12' , 'U' ) IS NOT NULL
+		DROP TABLE bronze.erp_cust_az12;
+
+		CREATE TABLE bronze.erp_cust_az12 (
+			cid NVARCHAR(50),
+			bdate DATE,
+			gen NVARCHAR(50)
+		);
+		IF OBJECT_ID ( 'bronze.erp_px_cat_g1v2' , 'U' ) IS NOT NULL
+		DROP TABLE bronze.erp_px_cat_g1v2;
+
+		CREATE TABLE bronze.erp_px_cat_g1v2 (
+			id NVARCHAR(50),
+			cat NVARCHAR(50),
+			subcat NVARCHAR(50),
+			maintenance NVARCHAR(50)
+		);
